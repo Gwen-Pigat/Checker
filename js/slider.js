b524851eb7175c74f90db57e5001fab5dee4ea87
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-02 19:03:20 +0200

    img/loading_1.GIF

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
 	$(".toggle").fadeIn(100);
+	$("#button").delay(2000).fadeIn(1000);
+	$("#main").show(1000);
+	$("#loading").delay(2000).fadeOut(100);
+
 
 	$(".toggle").click(function(){
 		$(".depenses").toggle(1000);
